32ebe7789941fc21bc2093d54e813dbbb2b7920f
TAnhpika <anhsheeran12345@gmail.com>
2026-04-30 17:26:19 +0700

    21.48 Tạo phương thức switch và destroy

--- src/tabzy.js
+++ src/tabzy.js
@@ -26,32 +26,68 @@ function Tabzy(selector) {
     // check thiếu panel
     if (this.tabs.length !== this.panels.length) return;
 
+    this._originHTML = this.container.innerHTML;
+
     this._init();
 }
 
 Tabzy.prototype._init = function () {
-    const tabActive = this.tabs[0];
-    tabActive.closest("li").classList.add("tabzy--active");
-
-    this.panels.forEach((panel) => (panel.hidden = true));
+    this._activeTab(this.tabs[0]);
 
     this.tabs.forEach((tab) => {
         tab.onclick = (event) => this._handleTabClick(event, tab);
     });
-
-    const panelActive = this.panels[0];
-    panelActive.hidden = false;
 };
 
 Tabzy.prototype._handleTabClick = function (event, tab) {
     event.preventDefault(); // k hiện trên url && k nhảy đến id
 
+    this._activeTab(tab);
+};
+
+Tabzy.prototype._activeTab = function (tab) {
     this.tabs.forEach((tab) => {
         tab.closest("li").classList.remove("tabzy--active");
-        
     });
+
+    tab.closest("li").classList.add("tabzy--active");
+
     this.panels.forEach((panel) => (panel.hidden = true));
 
-    const panelActive = document.querySelector(tab.getAttribute("href"))
-    panelActive.hidden = false
+    const panelActive = document.querySelector(tab.getAttribute("href"));
+    panelActive.hidden = false;
+};
+
+Tabzy.prototype.switch = function (input) {
+    let tabToActive = null;
+
+    if (typeof input === "string") {
+        // "#tab3"
+        tabToActive = this.tabs.find(
+            (tab) => tab.getAttribute("href") === input,
+        );
+
+        if (!tabToActive) {
+            console.error(`Tabzy: No panel found with ID '${input}'`);
+            return;
+        }
+    } else if (this.tabs.includes(input)) {
+        // tab3
+        tabToActive = input;
+    }
+
+    if (!tabToActive) {
+        console.error(`Tabzy: Invalid input '${input}'`);
+        return;
+    }
+
+    this._activeTab(tabToActive);
+};
+
+Tabzy.prototype.destroy = function () {
+    this.container.innerHTML = this._originHTML;
+    this.panels.forEach((panel) => (panel.hidden = false));
+    this.container = null
+    this.tabs= null
+    this.panels = null
 };
